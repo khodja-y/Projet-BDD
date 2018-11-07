9c28884d4bd4f3d4971567f7a7f9ff86d837495d
yasmine-khodja <44261817+yasmine-khodja@users.noreply.github.com>
2018-11-07 16:12:46 +0100

    Update rempl.sql

--- rempl.sql
+++ rempl.sql
@@ -63,6 +63,12 @@ INSERT INTO utilisateur_dim (id, nom, prenom, pseudo, anneeNaissance, sexe, stat
 VALUES (9, 'MANTOVAN', 'ELENA', 'ME', 1987, 'F', 'debutant', 2, 0, 1, 0);
 INSERT INTO utilisateur_dim (id, nom, prenom, pseudo, anneeNaissance, sexe, statue, niv_bavard, niv_fumeur, niv_animaux, niv_musique)
 VALUES (10, 'RYDH', 'DAVID', 'AL', 1986, 'F', 'debutant', 0, 1, 1, 1);
+INSERT INTO utilisateur_dim (id, nom, prenom, pseudo, anneeNaissance, sexe, statue, niv_bavard, niv_fumeur, niv_animaux, niv_musique)
+VALUES (11, 'ELHADI', 'SALAH', 'DI', 1995, 'H', 'debutant', 1, 1, 0, 1);
+INSERT INTO utilisateur_dim (id, nom, prenom, pseudo, anneeNaissance, sexe, statue, niv_bavard, niv_fumeur, niv_animaux, niv_musique)
+VALUES (12, 'CLAUDE', 'LAURENT', 'CL', 1980, 'H', 'habitue', 0, 1, 0, 1);
+INSERT INTO utilisateur_dim (id, nom, prenom, pseudo, anneeNaissance, sexe, statue, niv_bavard, niv_fumeur, niv_animaux, niv_musique)
+VALUES (13, 'JULIE-ANNE', 'LIEVRE', 'JA', 1988, 'F', 'ambassadeur', 2, 0, 0, 1);
 
 INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
 VALUES (1, 'Montpellier', 56.88, 277639, 34, 'Occitanie');
@@ -74,12 +80,41 @@ INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
 VALUES (4, 'Lyon', 47.87, 513275, 69, 'Auvergne-Rhone-Alpes');
 INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
 VALUES (5, 'Toulouse', 118.30, 471941, 31, 'Occitanie');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (6, 'NICE', 71.92, 340735, 06, 'Porvence-Alpes-Côte Azur');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (7, 'Nantes', 65.19, 282047, 44, 'Pays de la Loire');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (8, 'Strasbourg',78.29, 271708, 67, 'Alsace');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (9, 'Bordeaux', 49.36, 236725, 33, 'Aquitaine');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (10, 'Lille', 34.83, 226827, 59, 'Nord-Pas-de-Calais');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (11, 'Rennes',50.39, 206604, 35, 'Bretagne');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (12, 'Reims',49.90, 180842, 51, 'Champagne-Ardenne');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (13, 'Le havre',46.95, 177259, 76, 'Haute-Normandie');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (14, 'Saint-Etienne',79.97, 171961, 42, 'Rhône-Alpes');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (15, 'Toulon',42.83, 165514, 83, 'Provence-Alpes-Côte Azur');
+INSERT INTO ville_dim (id, nom, superficie, population, departement, region)
+VALUES (8, 'Strasbourg',78.29, 271708, 67, 'Alsace');
 
+
+
+
+	
+	
+	
+	
 -- /*************************
 -- RESERVATION FACT INSERTION
 -- **************************/
 
-INSERT INTO reservation (id_dateResa, id_heureResa, id_villeDep, id_villeArr, id_dateDep, id_passager, id_conducteur, num_trajet, nombrePlace, prixPlace, prixTotal)
+INSERT INTO reservation (id_dateResa, id_heureResa, id_villeDep, id_villeArr, id_dateDep, id_passager, id_conducteur, num_trajet, nombrePlace, complet, deux_places_ar, only_girls, prixPlace, prixTotal)
 VALUES ();
 
 -- /*************************
