c7327b8ea121cba5b2b476d72105df1e7ade9aeb
Terki <mohamed-adel.terki@etu.umontpellier.fr>
2018-11-08 12:18:01 +0100

    requetes update

--- requetes.sql
+++ requetes.sql
@@ -4,45 +4,52 @@
 --et le comparer au nombre de trajets rechercher 
 --selon la même ville de départ et de destination.
 
-SELECT 	ville_dim1.nom,ville_dim2.nom,
-		COUNT(proposition.id_date,proposition.id_dateDep,proposition.id_heureDep,proposition.id_villeDep,proposition.id_villeArr,proposition.id_conducteur) AS nbr_trajets_prop,
-		COUNT(recherche.id_date,recherche.id_dateDep,recherche.id_heureDep,recherche.id_villeDep,recherche.id_villeArr,recherche.id_conducteur)AS nbr_trajets_rech
-FROM ville_dim1,ville_dim2,recherche,proposition 
-WHERE 		ville_dim1.id=recherche.id_villeDep
-	AND 	ville_dim2.id=recherche.id_villeArr
-	AND	ville_dim1.id=proposition.id_villeDep
-	AND 	ville_dim2.id=proposition.id_villeDep	
-GROUP BY ville_dim1,ville_dim2,nbr_trajets_prop,nbr_trajets_rech;
+--on a besoin d'une dimensions dégénrées dans les tables de fait pour cette requete comme "numéro de recherche"
+
+
+CREATE MATERIALIZED VIEW MV4
+AS SELECT 	vd1.nom,vd2.nom,
+		COUNT(proposition.id_date||proposition.id_dateDep||proposition.id_heureDep||proposition.id_villeDep||proposition.id_villeArr||proposition.id_conducteur) AS nbr_trajets_prop,
+		COUNT(recherche.id_date||recherche.id_dateDep||recherche.id_heureDep||recherche.id_villeDep||recherche.id_villeArr||recherche.id_utilisateur) AS nbr_trajets_rech
+FROM        ville_dim vd1,ville_dim vd2,recherche,proposition 
+WHERE 		vd1.id=recherche.id_villeDep
+	AND 	vd2.id=recherche.id_villeArr
+	AND	    vd1.id=proposition.id_villeDep
+	AND 	vd2.id=proposition.id_villeArr	
+GROUP BY vd1.nom,vd2.nom;
 
 
 --Requête 6: Afficher le nombre de cartes carburant et 
 --le nombre de cartes lavage prises par les utilisateurs 
 --ayant proposés un premier trajet et comparer les deux nombres.
 
-SELECT COUNT(utilisateur1.cadeau),COUNT(utilisateur2.cadeau)
-FROM utilisateur,proposition
-WHERE 		utilisateur1.cadeau='LAVAGE'
-	AND	utilisateur2.cadeau='CARBURANT'
-	AND	utilisateur1.id=proposition.id_conducteur
-	AND  	utilisateur2.id=proposition.id_conducteur;
-	
+CREATE MATERIALIZED VIEW MV6
+AS SELECT COUNT(u1.cadeau) AS LAVAGE,COUNT(u2.cadeau) AS CARBURANT
+FROM    utilisateur_dim u1,utilisateur_dim u2,proposition
+WHERE 	u1.cadeau='LAVAGE'
+	AND	u2.cadeau='CARBURANT'
+	AND	u1.id=proposition.id_conducteur
+	AND u2.id=proposition.id_conducteur;
 --2eme solution plus correcte
-SELECT *
+CREATE MATERIALIZED VIEW MV6
+AS SELECT *
 FROM (SELECT count(utilisateur_dim.id) as nbr_lavage
       FROM utilisateur_dim
-      WHERE cadeau = 'LAVAGE' ) as LAVAGE, (SELECT count(utilisateur_dim.id) as nbr_carburant
+      WHERE cadeau = 'LAVAGE' ), (SELECT count(utilisateur_dim.id) as nbr_carburant
                                  FROM utilisateur_dim
-                                 WHERE cadeau = 'CARBURANT' ) as CARBURANT;
+                                 WHERE cadeau = 'CARBURANT' );
 
 --requête 2:
 -- Afficher le nombre de trajets réservés ayant le prix minimum en effectuant un group by sur la ville de départ et la ville de destination.
-select id_villeDep, id_villeArr, MIN(prix_place)
+CREATE MATERIALIZED VIEW MV2
+AS select id_villeDep, id_villeArr, MIN(prix_place)
     from reservation
     group by id_villeDep,id_villeArr;
     
 --requête 8:
 --Afficher le nombre de trajets réservés complets selon la ville de départ et ville d'arrivée
-select id_villeDep, id_villeArr, COUNT(num_trajet)
+CREATE MATERIALIZED VIEW MV8
+AS select id_villeDep, id_villeArr, COUNT(num_trajet)
 from reservation
 where complet = '1'
 group by id_villeDep, id_villeArr;
@@ -58,8 +65,8 @@ group by id_villeDep, id_villeArr;
 
 -- requête 3:
 -- Nombre de places vendues et prix total des réservations avec Ouibus par jour  
-
-SELECT d.fullDate, SUM(nombrePlace) as placeVendue, SUM(prixTotal) as prixTotal
+CREATE MATERIALIZED VIEW MV3
+AS SELECT d.fullDate, SUM(nombrePlace) as placeVendue, SUM(prixTotal) as prixTotal
 FROM reservation r, date_dim d
 WHERE r.id_conducteur = 10
 	AND r.id_dateResa = d.id
